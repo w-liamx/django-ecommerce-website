4b99ef70639a0be0545153d761c9f71552982904
liam <kayceewilliams10@gmail.com>
2020-01-15 13:53:01 +0100

    updated settings

--- cstore/settings.py
+++ cstore/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'False')
 
-ALLOWED_HOSTS = ['bannystores.herokuapp.com']
+ALLOWED_HOSTS = ['bannystores.herokuapp.com', '.herokuapp.com']
 
 
 # Application definition
