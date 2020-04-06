322bace1fd5ff23d7492ce53e041d06e6f2e0aa2
liam <kayceewilliams10@gmail.com>
2020-04-06 13:08:18 +0100

    Modified settings

--- cstore/settings.py
+++ cstore/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n6!qxb41showr8o1@)#=u)+_aj%1nb()k76^@a@te4!g*s^g$t'
+# SECRET_KEY = 'n6!qxb41showr8o1@)#=u)+_aj%1nb()k76^@a@te4!g*s^g$t'
 
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = (os.environ.get('DEBUG_VALUE') == 'False')
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'False')
+# DEBUG = True
 
 ALLOWED_HOSTS = ['bannystores.herokuapp.com', '127.0.0.1']
 
@@ -148,24 +148,18 @@ MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = None
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
 
-# DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 PRODUCTS_PER_ROW = 8
 
 django_heroku.settings(locals())
-
-# STRIPE_SECRET_KEY = "sk_test_cc3KvKhaDYSc23pTm6dX9zLy00lPGOVMUc"
-# STRIPE_PUB_KEY = "pk_test_2YHXf3xyzGEMyC6uQb6XWROT00jkxET4OR"
-
-# RAVEPAY_PUBLIC_KEY="FLWPUBK_TEST-1dc2013dd6472b8717782eebc13d72f8-X"
-# RAVEPAY_SECRET_KEY="FLWSECK_TEST-482b42482782b99c473b5c697e13c585-X"
 
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_PUB_KEY = os.environ.get('STRIPE_PUB_KEY')
